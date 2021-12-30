319e43a12ffa160f0ecfe184a5337bebb42e85db
Bahadur Alam <bahaduralam@gmail.com>
2021-12-30 18:27:45 +0600

    feat: implement create auction

---
+++ packages/auction/src/handlers/createAuction.js
@@ -0,0 +1,19 @@
+import { v4 as uuid } from 'uuid';
+
+async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+
+  const auction = {
+    id: uuid(),
+    title,
+    status: 'OPEN',
+    createdAt: new Date().toISOString(),
+  };
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = createAuction;
