22d5ab8e1b7c6fe7f7d1379a14318c2f29056bb0
Bahadur Alam <bahaduralam@gmail.com>
2022-01-18 14:20:09 +0600

    feat: implement lambda authorizer

---
+++ packages/auth/src/handlers/public.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+export async function handler(event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      // Required for CORS support to work
+      'Access-Control-Allow-Origin': '*',
+      // Required for cookies, authorization headers with HTTPS
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      message: 'Hello from Public API',
+    }),
+  };
+}
